95292ae91db0586c5ef2ba1b750c1ff586520edf
musaab-khan <musaabkhan4511@gmail.com>
2024-12-31 03:19:32 +0500

    using bcryptjs instead of bcrypt

--- api/index.js
+++ api/index.js
@@ -1,8 +1,2 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/endpoint', (req, res) => {
-  res.json({ message: 'This is your API endpoint' });
-});
-
-module.exports = router;
+const app = require('../server');
+module.exports = app;
